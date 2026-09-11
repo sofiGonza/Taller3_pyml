384970476f0b50f91b845e07173f1a6639d81038
sofiGonza <sofiagonzalezgallego777@gmail.com>
2026-09-11 12:13:25 -0500

    fix: configurar frontend

--- Modelos_ML/RegresionLineal/front/config/settings.py
+++ Modelos_ML/RegresionLineal/front/config/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://frontend-production-60d5d.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://frontend-production-4471.up.railway.app']
 
 
 # Application definition
